cd0f3872dfe5c8f96228bb556e8b8c6d2da1d18a
Ahmed H. <ahmedabdelgadir7796@gmail.com>
2026-02-08 05:23:22 +0300

    Fixed insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "test" ]]
 then
-  PSQL="psql --username=postgres --dbname=wc_test -t --no-align -c"
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
